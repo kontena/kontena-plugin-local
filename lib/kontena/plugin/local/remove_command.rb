a1cad831557f3bc018ecdda5a59c0fda573ab3e6
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-12-29 12:30:14 +0200

    remove deployed items on platform removal

--- lib/kontena/plugin/local/remove_command.rb
+++ lib/kontena/plugin/local/remove_command.rb
@@ -8,6 +8,11 @@ class Kontena::Plugin::Local::RemoveCommand < Kontena::Command
   def execute
     confirm_command('local-kontena') unless forced?
 
+    remove_stacks
+    remove_services
+    remove_volumes
+    wait_for_remove_complete
+
     remove_container('kontena-master-api')
     remove_container('kontena-master-mongo')
     remove_container('kontena-agent')
@@ -21,6 +26,36 @@ class Kontena::Plugin::Local::RemoveCommand < Kontena::Command
     remove_volume('kontena-master-db')
 
     Kontena.run('master rm --force --silent local-kontena')
+  end
+
+  def wait_for_remove_complete
+    spinner "Waiting for removals to complete" do
+      sleep 5
+    end
+  end
+
+  def remove_stacks
+    spinner "Removing stacks" do
+      client.get("grids/local-kontena/stacks")['stacks'].each do |stack|
+        client.delete("stacks/#{stack['id']}")
+      end
+    end
+  end
+
+  def remove_services
+    spinner "Removing services" do
+      client.get("grids/local-kontena/services")['services'].each do |service|
+        client.delete("services/#{service['id']}")
+      end
+    end
+  end
+
+  def remove_volumes
+    spinner "Removing volumes" do
+      client.get("volumes/local-kontena")['volumes'].each do |volume|
+        client.delete("volumes/#{volume['id']}")
+      end
+    end
   end
 
   def remove_container(name)
